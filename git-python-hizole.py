09f184bbbe4ae83994519b4db20e55aa823b6383
HizoleBryanJay <bryanhizole2001@gmail.com>
2023-03-08 10:25:29 +0800

    My Educational Background added

--- git-python-hizole.py
+++ git-python-hizole.py
@@ -1,3 +1,6 @@
 print ("Name: Bryan Jay J. Hizole")
 print ("Adress: Bunganay,Boac,Marinduque")
 print ("Birthday: November 14 2001")
+print ("College: Marinduque State College")
+print ("Course: Bachelor of Science in Information Technology")
+print ("Year/Section: 2C")
